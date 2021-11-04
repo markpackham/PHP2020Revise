14b3bfa208cdc2fa1c5477ad5707296d871abe55
mark <markpackham1@gmail.com>
2021-11-04 09:08:24 +0000

    using nl2br() for \n and \r
    https://youtu.be/lp78kFc347Y?t=1

--- echo.php
+++ echo.php
@@ -3,3 +3,5 @@
 $name = "Dary";
 echo "My name is {$name} <br>";
 echo 'My name is {$name}';
+echo "<br>";
+print "Print can only handly one string so concatinaiton is needed "."a period (.) and must be used and not a comma (,)";
---
+++ linebreak.php
@@ -0,0 +1,5 @@
+<?php
+
+echo nl2br("My name is bob \r\n and I'm a horse");
+echo "I live in a volcano <br>";
+echo "I like eating sheep"."<br>";
