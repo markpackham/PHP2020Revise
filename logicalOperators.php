14b04e2da9fdaf6e558c2505ef252a188d45d8fe
mark <markpackham1@gmail.com>
2021-11-05 14:33:12 +0000

    demo xor (even though I'll probably never use it)
    https://www.youtube.com/watch?v=fB73u7OmFu8&list=PLFHz2csJcgk_fFEWydZJLiXpc9nB1qfpi&index=21

---
+++ logicalOperators.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = 5;
+$y = 10;
+
+// gives true, ONLY check 1 and no more Must be True for xor to give True
+if ($x == $y xor 1 != 2) {
+    echo "True";
+} else {
+    echo "False";
+}
