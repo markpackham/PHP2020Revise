a834741f0ef70879518d2431875fc367b08e655d
mark <markpackham1@gmail.com>
2021-11-05 16:03:35 +0000

    use foreach
    https://www.youtube.com/watch?v=oemvQkynx3I&list=PLFHz2csJcgk_fFEWydZJLiXpc9nB1qfpi&index=28

---
+++ foreach.php
@@ -0,0 +1,13 @@
+<?php
+
+// $names = ["Bob", "Frank", "Greg"];
+
+// foreach ($names as $name) {
+//     echo $name . " ";
+// }
+
+$persons = ["Name" => "Bill", "Age" => 20, "Sex" => "Male"];
+
+foreach ($persons as $key => $value) {
+    echo "$key:$value,";
+}
