f5783d9ff80d1410c07a02ebfde349a8ecf92f31
mark <markpackham1@gmail.com>
2021-11-08 13:51:12 +0000

    add common maths functions
    https://www.youtube.com/watch?v=g3QgoYBgFHQ&list=PLFHz2csJcgk_fFEWydZJLiXpc9nB1qfpi&index=39

---
+++ mathBuiltIn.php
@@ -0,0 +1,21 @@
+<?php
+
+// 2
+echo ceil(1.50);
+echo " ";
+// 1
+echo floor(1.50);
+echo " ";
+// 1.51
+echo round(1.50511, 2);
+echo " ";
+// Calculates the exponent of e
+// 'e' is the base of the natural system of logarithms, or approximately 2.718282.
+// 54.598150033144
+echo exp(4);
+echo " ";
+// 3 to the power of 2 gives 9
+echo 3 ** 2;
+echo " ";
+// other way of writing power of in PHP
+echo pow(3, 2);
