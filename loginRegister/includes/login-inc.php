2967f9bde913caaee2634194280da9d56e8cfe60
mark <markpackham1@gmail.com>
2021-11-10 14:27:22 +0000

    connect login page to database
    https://youtu.be/xf4Sz87xZvI?t=3506

--- loginRegister/includes/login-inc.php
+++ loginRegister/includes/login-inc.php
@@ -1,1 +1,13 @@
 <?php
+
+if (isset($_POST['submit'])) {
+
+    require 'database.php';
+
+    // htmlentities protects us from JavaScript injections
+    $username = htmlentities($_POST['username']);
+    $password = htmlentities($_POST['password']);
+} else {
+    header("Location: ../loginRegister.php?error=accessforbidden");
+    exit();
+}
