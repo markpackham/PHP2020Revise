284daa28ef5d2383edde7bd03755e91ca083a912
mark <markpackham1@gmail.com>
2021-11-10 08:56:46 +0000

    output users
    https://youtu.be/xf4Sz87xZvI?t=699

--- loginRegister/loginRegister.php
+++ loginRegister/loginRegister.php
@@ -1,5 +1,29 @@
 <?php
+// Header has access to the database connection file
 require_once 'includes/header.php';
+?>
+
+<?php
+$sql = "SELECT * FROM users";
+$result = mysqli_query($connection, $sql);
+$rowCount = mysqli_num_rows($result);
+
+if ($rowCount > 0) {
+    echo "<ul>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<li>" . $row['username'] . "<br>" . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "<p>No results found.</p>";
+}
+
+
+if (isset($_SESSION['sessionId'])) {
+    echo "<p>You are logged in!</p>";
+} else {
+    echo "<p>Home Page</p>";
+}
 ?>
 
 <?php
